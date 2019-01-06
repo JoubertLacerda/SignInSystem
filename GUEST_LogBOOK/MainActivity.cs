46ba4affd735f3327591edeff6d7976203f23187
Joubert <Joubertlacerda@hotmail.com>
2019-01-07 00:34:49 +1000

    Add to Git
    added a copy of the SignInSystem to  Git.

---
+++ GUEST_LogBOOK/MainActivity.cs
@@ -0,0 +1,48 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Support.V7.App;
+using Android.Runtime;
+using Android.Widget;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Android.Content;
+
+namespace GUEST_LogBOOK
+{    
+
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    public class MainActivity : Activity
+    {
+        
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.activity_main);
+
+            SignIn();
+        }
+        public void SignIn()
+        {
+            //TODO:add the method for the sign in screen here, screen should contain a field the guest to type their name, and who they are visiting. a number should be automaticly assigned to each guest. the hour they signed in should also be saved. while the guest is signed in the information should be displayed on the 'Sign Out' screen.
+            var BTN_IN = FindViewById<Button>(Resource.Id.BTN_signIN);
+            BTN_IN.Click += delegate
+            {
+                StartActivity(typeof(SignINscreen));
+            };
+        }
+        public void SignOUT()
+        {
+            //TODO: add the method for the sign OUT screen here, should be in a GridView, user to select their name and click a button to sign out, GriView to grey out the entire row containing the name, date, number of the guest. the hour they signed out should be saved. all this info to be saved on a .TXT file
+            var BTN_OUT = FindViewById<Button>(Resource.Id.BTN_signOUT);
+            BTN_OUT.Click += delegate
+            {
+                StartActivity(typeof(SignOUTscreen));
+            };
+        }
+    }
+    
+}
---
+++ GUEST_LogBOOK/SignINscreen.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace GUEST_LogBOOK
+{   
+    [Activity(Label = "@string/SignIN")]
+    public class SignINscreen : Activity
+    {  
+        
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Create your application here
+            SetContentView(Resource.Layout.SignIN_Screen);
+            BackBTN();
+        }
+
+        public void BackBTN()
+        {
+            //clicking the back button to go to main screen.
+            var BackBTN = FindViewById<Button>(Resource.Id.BTN_Back);
+            BackBTN.Click += delegate
+            {
+                StartActivity(typeof(MainActivity));
+            };
+        }
+        public void GuestRegister()
+        {
+            //First Name.
+            var Edit_Fname = FindViewById<EditText>(Resource.Id.TxtFname); 
+            //Last name.
+            var Edit_Lname = FindViewById<EditText>(Resource.Id.TxtLName);
+            //who is the guest here to see?.
+            var Edit_HereToSee = FindViewById<EditText>(Resource.Id.TxtHereToSeeWho);
+            //Visitor number,TODO this to be auto asigned
+            var GuestNo = FindViewById<TextView>(Resource.Id.TxtGuestNo);
+            //Time Visitor arrived.
+            var TimeIn = FindViewById<TextView>(Resource.Id.TimeIn);
+
+           
+                Intent RegisterUser = new Intent(this, typeof(SignOUTscreen));
+            RegisterUser.PutExtra("First Name", Edit_Fname.Text);
+            RegisterUser.PutExtra("Last Name", Edit_Lname.Text);
+            RegisterUser.PutExtra("Here to See", Edit_HereToSee.Text);
+            RegisterUser.PutExtra("Guest No.", GuestNo.Text);
+            RegisterUser.PutExtra("Arrived At", TimeIn.Text);
+            
+
+            
+            
+        }
+    }
+}
